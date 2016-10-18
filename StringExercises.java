a7c7f63ee5822540a341610f46801e30e5a87176
Josh Orndorff <jorndorff@pingry.org>
2016-10-18 13:19:15 -0400

    Initial commit. Starter code (hopefully) complete.

---
+++ StringExercises.java
@@ -0,0 +1,57 @@
+/**
+ * Finds the first instance of s in line,
+ * starting at the position start
+ *
+ * @param o the orignal string 
+ * @param s the string to find 
+ * @param start the position to start searching. Precondition: 
+ * Guaranteed to be less than the string length.
+ *
+ * @return the index of the first single instance of
+ * s if the string is found OR -1 if it is not found.
+ */
+public static int findString (String o, String s, int start)
+{  //TODO Implement this
+
+}
+
+
+/**
+ * Count the number of times instances of s appear in the line.
+ *
+ * @param o the original string
+ * @param s the string to find.
+ *
+ * @return the number of times the string appears in the line
+ */
+public static int countStrings (String o, String s)
+{  //TODO Implement this
+
+}
+
+
+/**
+ * Replace all instances of underscores in the line given by
+ * line with italics tags.  There must be an even number of underscores
+ * in the line, and they will be replaced by <I>, </I>, alternating.
+ * @param line a string with 0 or more underscores
+ * @return the line that has underscores replaced with <I> </I> tags or 
+ * the original line if there are not an even number of underscores.
+ */
+public static String convertItalics (String line)
+{
+  /* 
+  Here are some examples
+  line                             value returned by convertItalics
+  
+  "This is _very_ good."           "This is <I>very</I> good."
+  "_This_ is _very_ _good_."       "<I>This</I> is <I>very</I> <I>good</I>."
+  "This is _very good."            "This is _very good."
+  "This is __very good."           "This is __very good."
+  */
+
+  //TODO Implement this
+
+}
+  
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+String Exercises
+================
+
+This class has three methods that operate on strings in interesting (but difficult) ways.
+
+findString
+----------
+
+countStrings
+------------
+
+convertItalics
+--------------
+
+Usage
+-----
+This program does not have a main method and therefore cannot be run as a standalone program.
+It can be compiled with: `javac StringExercises.java`
